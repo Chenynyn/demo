094843e8b7671bfb80afed677fa47a51494062b1
Chenynyn <chenyn9@asiainfo.com>
2022-08-24 16:56:12 +0800

    demo3

---
+++ src/main/java/com/example/demo3/pojo/Student.java
@@ -0,0 +1,13 @@
+package com.example.demo3.pojo;
+
+import lombok.Data;
+
+
+@Data
+public class Student {
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    private String student_age;
+}
---
+++ src/main/java/com/example/demo3/pojo/TestStudent.java
@@ -0,0 +1,11 @@
+package com.example.demo3.pojo;
+
+public class TestStudent {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setAge(10);
+
+    }
+}
+
+
