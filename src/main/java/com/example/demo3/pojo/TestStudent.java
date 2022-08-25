603e151ed5554d16c534ce478cae17b6c506312a
Chenynyn <chenyn9@asiainfo.com>
2022-08-25 10:48:30 +0800

    demo3

--- src/main/java/com/example/demo3/pojo/TestStudent.java
+++ src/main/java/com/example/demo3/pojo/TestStudent.java
@@ -4,7 +4,7 @@ public class TestStudent {
     public static void main(String[] args) {
         Student student = new Student();
         student.setAge(10);
-
+        System.out.println(123);
     }
 }
 
