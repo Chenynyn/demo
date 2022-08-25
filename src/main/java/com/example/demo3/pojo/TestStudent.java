16be73c946ac7691422ce980de9d86cde5f46049
Chenynyn <chenyn9@asiainfo.com>
2022-08-25 11:25:21 +0800

    Merge branch 'master' into newChange
    # Conflicts:
    #	src/main/java/com/example/demo3/pojo/TestStudent.java

--- src/main/java/com/example/demo3/pojo/TestStudent.java
+++ src/main/java/com/example/demo3/pojo/TestStudent.java
@@ -4,7 +4,9 @@ public class TestStudent {
     public static void main(String[] args) {
         Student student = new Student();
         student.setAge(10);
+        student.setName("小红");
         System.out.println(123);
+        System.out.println(student.getName());
     }
 }
 
