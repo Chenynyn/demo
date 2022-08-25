5de085b0ddd7e86002c43384e36928c2094e92d2
Chenynyn <chenyn9@asiainfo.com>
2022-08-25 14:15:05 +0800

    Merge branch 'secondChange' into HEAD
    # Conflicts:
    #	src/main/java/com/example/demo3/pojo/TestStudent.java

--- src/main/java/com/example/demo3/pojo/TestStudent.java
+++ src/main/java/com/example/demo3/pojo/TestStudent.java
@@ -6,6 +6,7 @@ public class TestStudent {
         student.setAge(10);
         student.setName("小红");
         System.out.println(123);
+        System.out.println(99999);
         System.out.println(student.getName());
 
     }
